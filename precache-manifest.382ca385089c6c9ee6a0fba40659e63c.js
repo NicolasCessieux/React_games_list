5437d5543f027967e129fe145c3e56a4f862b9df
NicolasCessieux <nicolascessieux@hotmail.fr>
2019-05-15 23:24:31 +0200

    Updates

---
+++ precache-manifest.382ca385089c6c9ee6a0fba40659e63c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad65dc43a0a52162b21608b8ce65bdc",
+    "url": "/react_games_list/index.html"
+  },
+  {
+    "revision": "b0970ff2576116529693",
+    "url": "/react_games_list/static/css/main.ee0cc453.chunk.css"
+  },
+  {
+    "revision": "610e85795d115b3dc4f1",
+    "url": "/react_games_list/static/js/2.22f8dd3a.chunk.js"
+  },
+  {
+    "revision": "b0970ff2576116529693",
+    "url": "/react_games_list/static/js/main.d2709da8.chunk.js"
+  },
+  {
+    "revision": "d8e0ab8471ce9be094ef",
+    "url": "/react_games_list/static/js/runtime~main.bdcc6ab4.js"
+  }
+]);
